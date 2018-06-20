a98cce915663372718c9d47e463753c3635ee425
Janis Rozenfelds <janis.rozenfelds@gmail.com>
2018-06-20 17:53:07 +0900

    deply 7

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	base: '/documentation-page-vuepress/',
-	title: 'VuePress Example in Github Pages',
+	title: 'VuePress Example Pages',
 	description: 'VuePress and deployed on Github Pages',
 	// dest: './docs',
 
