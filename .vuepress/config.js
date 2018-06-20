0def4d127d00f1ea64f139440f80fb32daff7bbc
Janis Rozenfelds <janis.rozenfelds@gmail.com>
2018-06-20 15:55:59 +0900

    deploy-2

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,27 @@
 module.exports = {
-	base: '/documentation-page-vuepress/',
+	base: '/Documentation-Page-Vuepress/',
 	title: 'VuePress Example in Github Pages',
 	description: 'VuePress and deployed on Github Pages',
-	dest: './docs'
+	dest: './docs',
+
+	themeConfig: {
+		// Assumes GitHub. Can also be a full GitLab url.
+		repo: 'vuejs/vuepress',
+		// Customising the header label
+		// Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+		repoLabel: 'Contribute!',
+
+		// Optional options for generating "Edit this page" link
+
+		// if your docs are in a different repo from your main project:
+		docsRepo: 'vuejs/vuepress',
+		// if your docs are not at the root of the repo:
+		docsDir: 'docs',
+		// if your docs are in a specific branch (defaults to 'master'):
+		docsBranch: 'master',
+		// defaults to false, set to true to enable
+		editLinks: true,
+		// custom text for edit link. Defaults to "Edit this page"
+		editLinkText: 'Help us improve this page!'
+	}
 }
